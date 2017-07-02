a42f9b3ce7a6ac52024a67ef9cb6711c19748a76
root <root@hacking-session>
2017-07-02 13:55:26 +0000

    Removed bad examples with BAAAD type names

--- grammar.ml
+++ grammar.ml
@@ -14,57 +14,5 @@ and typ =
   | TypeArrow of typ * typ
   | TypeRecord of (string * typ) list
 
-let base_string = TypeBase "String"
-let base_int = TypeBase "Integer"
-let base_unit = TypeBase "Unit"
-
 type raw_top_level_term =
   | Top_level_let of string * term
-
-(* Some examples. *)
-(*
-let print_infered_type term =
-  let type_of_term = typer [] term in
-  Printf.printf "%s\n" (string_of_typ type_of_term)
-
-let test0 = TermAbstraction("x", base_int, TermVariable("x"));;
-let test1 = TermAbstraction("x",
-                            base_string,
-                            TermAbstraction("y",
-                                            base_int,
-                                            TermApplication(
-                                              TermVariable("x"),
-                                              TermVariable("y")
-                                            )
-                                           )
-                           );;
-let test2 = TermAbstraction("x",
-                            TypeArrow(base_string, base_int),
-                            TermAbstraction("y",
-                                            base_int,
-                                            TermApplication(
-                                              TermVariable("x"),
-                                              TermVariable("y")
-                                            )
-                                           )
-                           );;
-let test3 = TermAbstraction("x",
-                            TypeArrow(base_int, base_string),
-                            TermAbstraction("y",
-                                            base_int,
-                                            TermApplication(
-                                              TermVariable("x"),
-                                              TermVariable("y")
-                                            )
-                                           )
-                           );;
-
-let test4 = TermRecord(
-    [("test0", test0) ; ("test3", test3)]
-  );;
-*)
-(* print_infered_type test0;; *)
-(* print_infered_type test1;; *)
-(* print_infered_type test2;; *)
-(* print_infered_type test3;; *)
-(* print_infered_type test4;; *)
